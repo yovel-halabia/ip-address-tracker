00610636cd6e368e578bfc462bb62c8af6267dca
yovel halabia <>
2021-04-08 19:47:07 +0300

    save

--- server.js
+++ server.js
@@ -1,49 +1,88 @@
 const express = require("express");
 const app = express();
 const https = require('https');
+const fetch = require("node-fetch");
 
   app.use(express.static('public'));
+  app.set("view engine","ejs");
 
   var ip;
   var data;
+  var defaultData;
 
+  const getData = async(getip)=>{
+    if(!getip){
+      await fetch("https://api.ipify.org?format=json")
+      .then((res)=>{return res.json();})
+      .then((data)=>{ip=data.ip.toString();})
+      .catch((err)=>{console.log(err)})
+    }else{
+      ip = getip;
+    }
 
-  //get ip
-  const getIP = new Promise((resolve, reject) =>{
-      https.get("https://api.ipify.org?format=json",(res)=>{
-      res.on('data', (d) => {
-        ip = JSON.parse(d).ip.toString();
-        resolve();
-      });
+    await fetch("https://geo.ipify.org/api/v1?apiKey=at_UxFRkmX62TqZNFxL3xakCNUumBIf7&ipAddress="+ip)
+    .then((res)=>{return res.json();})
+    .then((resData)=>{data=resData})
+    .catch((err)=>{console.log(err)})
+    
+  }
 
-      res.on('error',()=>{
-        reject();
-      })
-    });
-  });
-
-  //get the location after getting the ip
-  getIP.then(()=>{
-    console.log(ip);
-    https.get("https://geo.ipify.org/api/v1?apiKey=at_UxFRkmX62TqZNFxL3xakCNUumBIf7&ipAddress="+ip,(res)=>{
-      res.on('data', (d) => {
-        data= JSON.parse(d);
-      });
-    });
-  }).catch(()=>{console.log("err");});
 
 
 
     
 
   app.get("/",(req, res)=>{
-    res.sendFile(__dirname + "/index.html");
-  })
+    getData().then(()=>{
+      defaultData = data;
+      res.render("index", {
+          ip: ip,
+          location:data.location.city,
+          timezone:data.location.timezone,
+          isp:data.isp,
+          lat: data.location.lat,
+          lng:data.location.lng,
+          validation:false
+      });  
+    })
+    
+  });
+
+
+  app.get("/:ip",(req, res)=>{  
+    getData(req.params.ip).then(()=>{
+      if(!data.ip){
+        res.render("index", { 
+          ip: defaultData.ip,
+          location:defaultData.location.city,
+          timezone:defaultData.location.timezone,
+          isp:defaultData.isp,
+          lat: defaultData.location.lat,
+          lng:defaultData.location.lng,
+          validation:true
+        });
+
+      }else{
+        res.render("index", { 
+          ip: ip,
+          location:data.location.city,
+          timezone:data.location.timezone,
+          isp:data.isp,
+          lat: data.location.lat,
+          lng:data.location.lng,
+          validation:false
+        });
+      }
+      
+    })
+  });
 
 
 
 
-app.listen(3000, () => {
+
+
+app.listen(process.env.PORT||3000, () => {
   console.log("listen 3000");
 });
 
